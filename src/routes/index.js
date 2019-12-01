f2cf140004ffb2ea8d84d1655eede5ef33d38857
Leonardo Barbosa <leonardo.pb@icloud.com>
2019-12-01 18:34:08 -0300

    Configuracao de Rotas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import Students from '../pages/Students';
@@ -11,10 +12,10 @@ export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
-      <Route path="/students" component={Students} />
-      <Route path="/plans" component={Plans} />
-      <Route path="/registration" component={Registration} />
-      <Route path="/helporders" component={HelpOrders} />
+      <Route path="/students" component={Students} isPrivate />
+      <Route path="/plans" component={Plans} isPrivate />
+      <Route path="/registration" component={Registration} isPrivate />
+      <Route path="/helporders" component={HelpOrders} isPrivate />
     </Switch>
   );
 }
